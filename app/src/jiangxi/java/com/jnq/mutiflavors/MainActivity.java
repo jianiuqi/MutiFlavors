218f976dd0cdac1a27dafd1033985b6c2c8224dc
jiaxiaoniu <1174407577@qq.com>
2017-12-11 20:29:14 +0800

    change launcher icon

--- app/src/jiangxi/java/com/jnq/mutiflavors/MainActivity.java
+++ app/src/jiangxi/java/com/jnq/mutiflavors/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jnq.mutiflavors;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "jx_toast", Toast.LENGTH_SHORT).show();
     }
 }
